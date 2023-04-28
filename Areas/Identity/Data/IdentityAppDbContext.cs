c465d87d4bba2ebf3f25993f404fec808d0d848c
= <nurzhan.aytimov@gmail.com>
2023-04-28 18:10:36 +0600

    IdentityUsers
    a simple program for registering and authorizing a user

---
+++ Areas/Identity/Data/IdentityAppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using IdentityProject.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdentityProject.Areas.Identity.Data;
+
+public class IdentityAppDbContext : IdentityDbContext<Users>
+{
+    public IdentityAppDbContext(DbContextOptions<IdentityAppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new AppIdentityConfiguration());
+    }
+
+
+}
+
+public class AppIdentityConfiguration : IEntityTypeConfiguration<Users>
+{
+    public void Configure(EntityTypeBuilder<Users> builder)
+    {
+        builder.Property(u => u.Name).HasMaxLength(100);
+        builder.Property(u => u.LastName).HasMaxLength(100);
+    }
+}
